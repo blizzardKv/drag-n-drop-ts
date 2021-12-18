29d88e2f5439b51544d89f71628a3a807c6d2ae9
blizzardKv <makemewanna@yandex.ru>
2021-12-18 21:51:11 +0300

    code splitting

---
+++ src/components/project-input.ts
@@ -0,0 +1,74 @@
+/// <reference path="base-component.ts" />
+
+namespace App {
+  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor() {
+      super('project-input', 'app', true, 'user-input');
+
+      this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title');
+      this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description');
+      this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people');
+
+      this.configure();
+    }
+
+    configure(): void {
+      this.element.addEventListener('submit', this.submitHandler);
+    }
+
+    renderContent() {}
+
+    @autobind
+    private submitHandler(event: Event): void {
+      event.preventDefault();
+      const userInput = this.gatherUserInput();
+
+      if (Array.isArray(userInput)) {
+        const [title, description, people] = userInput;
+        projectState.addProject(title, description, people);
+        this.clearInputs();
+      }
+    }
+
+    private clearInputs():void {
+      this.titleInputElement.value = '';
+      this.descriptionInputElement.value = '';
+      this.peopleInputElement.value = '';
+    }
+
+    private gatherUserInput(): void | [string, string, number] {
+      const enteredTitle = this.titleInputElement.value;
+      const enteredDescription = this.descriptionInputElement.value;
+      const enteredPeople = this.peopleInputElement.value;
+
+      const titleValidatable: Validatable = {
+        value: enteredTitle,
+        required: true,
+      };
+
+      const descriptionValidatable: Validatable = {
+        value: enteredDescription,
+        required: true,
+        minLength: 3,
+      };
+
+      const peopleValidatable: Validatable = {
+        value: enteredTitle,
+        required: true,
+        min: 1,
+        max: 5,
+      };
+
+      if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
+        alert('input error!');
+        return;
+      }
+
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+}
---
+++ src/state/project-state.ts
@@ -0,0 +1,62 @@
+namespace App {
+  type Listener<T> = (items: T[]) => void;
+
+  class State<T> {
+    protected listeners: Listener<T>[] = [];
+
+    addListener(listenerFn: Listener<T>) {
+      this.listeners.push(listenerFn);
+    }
+  }
+
+  export class ProjectState extends State<Project>{
+    private projects: Project[] = [];
+    private static instance: ProjectState;
+
+    private constructor() {
+      super();
+    }
+
+    static getInstance() {
+      if (this.instance) {
+        return this.instance;
+      }
+
+      this.instance = new ProjectState();
+      return this.instance;
+    }
+
+    addProject(title: string, description: string, numberOfPeople: number) {
+      const newProjects = new Project(
+        Math.random().toString(),
+        title,
+        description,
+        numberOfPeople,
+        ProjectStatus.Active
+      );
+
+      this.projects.push(newProjects);
+
+      for (const listenerFn of this.listeners) {
+        listenerFn(this.projects.slice());
+      }
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus): void {
+      const project = this.projects.find((prj) => prj.id === projectId);
+
+      if (project && project.status !== newStatus) {
+        project.status = newStatus;
+        this.updateListeners();
+      }
+    }
+
+    private updateListeners() {
+      for (const listenerFn of this.listeners) {
+        listenerFn(this.projects.slice());
+      }
+    }
+  }
+
+  export const projectState = ProjectState.getInstance();
+}
